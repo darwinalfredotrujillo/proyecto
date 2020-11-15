3bdb15a0e74ec608bf3018a7aca9d8438bfb64b1
darwinalfredotrujillo <71801446+darwinalfredotrujillo@users.noreply.github.com>
2020-11-15 01:24:28 -0600

    Se añadió menu a la vista

--- resources/views/supervisor/categoria/index.blade.php
+++ resources/views/supervisor/categoria/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layout.master')
 @section('title', 'ADMINISTRADOR')
+
+@section('menu')
+<ul class="nav menu">
+    <li class="active">
+        <a href="#"><em class="fa fa-dashboard">&nbsp;</em>Sucesos</a>
+    </li>
+    <li><a href="#"><em class="fa fa-calendar">&nbsp;</em>PRPUESTAS</a></li>
+    <li><a href="#"><em class="fa fa-bar-chart">&nbsp;</em>PREGUNTAS</a></li>
+    <li><a href="#"><em class="fa fa-toggle-off">&nbsp;</em>ESTADO DE CUENTA</a></li>
+    <li><a href="#"><em class="fa fa-power-off">&nbsp;</em> LOGOUT</a></li>
+</ul>
+@endsection
+
 @section('content')
 <div class="panel shadow">
   <div class="inside">
